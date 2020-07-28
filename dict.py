99118f4aeac2bb016795361499a6ed6f8d687c1c
Edex01 <jorasiac@gmail.com>
2020-07-28 14:15:36 +0530

    Tkinter setup

--- dict.py
+++ dict.py
@@ -1,34 +1,65 @@
 import json
 from difflib import get_close_matches
+from tkinter import *
+from tkinter import messagebox
 
-data = json.load(open("data.json"))
+root = Tk()
+root.title = "Dictionary"
 
+word = StringVar()
 
-word = input("Enter word here: ")
+# exit button
+ext = Button(root , text = "Exit", command = root.destroy)
+ext.pack()
 
-def translate(word):
-    if word in data:
-        return data[word]
-    elif word.title() in data:
-        return data[word.title()]
-    elif word.upper() in data:
-        return data[word.upper()]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        print("did you mean %s instead" %get_close_matches(word, data.keys()[0]))
+# enter word Label
+lbl = Label(root, text = "Enter word")
+lbl.pack(side = LEFT)
+
+# Word Input
+ent = Entry(root, bd = 5, textvariable = word)
+ent.pack(side = RIGHT)
+ent.focus()
+
+#=================================================================================================
+def translate():
+
+    global word
+
+    if word.get() in data:
+        return data[word.get()]
+    elif word.get().title() in data:
+        return data[word.get().title()]
+    elif word.get().upper() in data:
+        return data[word.get().upper()]
+    elif len(get_close_matches(word.get(), data.keys())) > 0:
+        print("did you mean %s instead" %get_close_matches(word.get(), data.keys()[0]))
         decide = input("y for yes or n for no")
         if decide == "y":
-            return data[get_close_matches(word, data.keys())[0]]
+            return data[get_close_matches(word.get(), data.keys())[0]]
         elif decide == "n":
             return ("maybe you have misspelled word!")
         else:
             return ("wrong input!!!")
+
     else:
         return ("check spelling and try again!")
+#========================================================================================================
 
-output = translate(word)
-print("Meaning-->")
-if type(output)== list:
-    for item in output:
-        print(item)
-else:
-    print(output)
+data = json.load(open("data.json"))
+
+#========================================================================================================
+def result():
+    output = translate()
+    if type(output)== list:
+        for item in output:
+            messagebox.showinfo("Meaning",item)
+    else:
+        messagebox.showinfo("Meaning", output)
+#=========================================================================================================
+
+# search button
+search = Button(root , text = "Search", command = result)
+search.pack()
+
+root.mainloop()
